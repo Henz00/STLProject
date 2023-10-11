f78abeeda1ced299ec24397f086735911921416b
MagnusRoemer <maroe18@student.sdu.dk>
2023-10-11 16:43:09 +0200

    Fixed sprites og movement
    Barriere inkluderet, all good

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
 
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
 
-        movement.Normalize();
-
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
