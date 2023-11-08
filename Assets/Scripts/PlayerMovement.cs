ddf0b16fe40b27b377348d1503aba16737228433
Henz00 <henzoo.hansen@gmail.com>
2023-11-08 13:13:12 +0100

    Added horizontal flip for player character depending on direction of movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D body;
+    SpriteRenderer sprite;
 
     float horizontal;
     float vertical;
-    float moveLimiter = 0.7f;
+    float moveLimiter = Mathf.Sqrt(2)/2;
 
     public float runSpeed = 20.0f;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -23,15 +25,19 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
     }
-
+    
     void FixedUpdate()
     {
         if (horizontal != 0 && vertical != 0)
         {
-
             horizontal *= moveLimiter;
             vertical *= moveLimiter;
         }
+
+        if(horizontal > 0)
+            sprite.flipX = true;
+        else if (horizontal < 0)
+            sprite.flipX = false;
 
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
