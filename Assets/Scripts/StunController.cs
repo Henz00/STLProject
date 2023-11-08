4e196112bd8d068f389f6fcb2bcb6206b301e627
MagnusRoemer <maroe18@student.sdu.dk>
2023-11-08 13:43:18 +0100

    Enabled script
    mangler animation

--- Assets/Scripts/StunController.cs
+++ Assets/Scripts/StunController.cs
@@ -7,22 +7,12 @@ public class StunController : MonoBehaviour
     public float stunDuration = 2.0f;
     private bool isStunned = false;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-            StunController stunController = GetComponent<StunController>();
-            stunController.StunObject(other.gameObject);
-
-        }
-    }
     public void StunObject(GameObject target)
     {
         if (!isStunned)
         {
             isStunned = true;
-            // afspil stuneffect når asset findes (particles, sound)
-            // target.GetComponent<StunEffectScript>().PlayStunEffect();
+            //target.GetComponent<StunEffectScript>().PlayStunEffect();
 
             StartCoroutine(UnstunAfterDelay(target));
         }
