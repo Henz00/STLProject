b3c9af625a268191eb884263f68475416a9fbb87
xkhassoukx <Oskha18@student.sdu.dk>
2023-11-16 10:23:09 +0100

    Grass and Sheep Script Edited, AlignTargetAdded

--- Assets/Scripts/GrassController.cs
+++ Assets/Scripts/GrassController.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class GrassController : MonoBehaviour
 {
     private bool isTargeted = false;
+    public float disappearDelay = 2f;
+    public float grassSpawnDelay = 2f;
+    public bool isEnabled = true;
+
+    void Start()
+    {
+        gameObject.SetActive(isEnabled);
+        Invoke("GrassSpawnTime", grassSpawnDelay);
+    }
 
     public bool IsTargeted()
     {
@@ -12,5 +21,29 @@ public class GrassController : MonoBehaviour
     public void SetTargeted(bool value)
     {
         isTargeted = value;
+    }
+
+    private void GrassSpawnTime()
+    {
+        isEnabled = !isEnabled;
+        gameObject.SetActive(isEnabled);
+    }
+
+    private void DisappearDelayed()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sheep"))
+        {
+            SheepController sheep = other.GetComponent<SheepController>();
+            if (sheep != null)
+            {
+                sheep.SetIsTargeted(false); // Stop the sheep from targeting grass
+                Invoke("DisappearDelayed", disappearDelay);
+            }
+        }
     }
 }
--- Assets/Scripts/SheepController.cs
+++ Assets/Scripts/SheepController.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class SheepController : MonoBehaviour
 {
+    private bool reachedGoal;
     public string grassTag = "Grass";
     public float moveSpeed = 2f;
+    public float playerFollowDistance = 5f; // Adjust the distance as needed
 
     private Transform targetGrass;
+    private Transform alignToGoal;
+    private Transform walkToGoal;
+
     private bool isTargeted = false;
+    private MiniGameManager gameState;
+    
+
+    private void Awake()
+    {
+        gameState = GameObject.Find("GameManager").GetComponent<MiniGameManager>();
+        alignToGoal = GameObject.Find("AlignSheepTargetPoint").GetComponent<Transform>();
+        walkToGoal = GameObject.Find("SheepTargetPoint").GetComponent<Transform>();
+
+    }
 
     void Update()
     {
-        if (!isTargeted)
+        
+
+        if(reachedGoal)
         {
-            FindNearestAvailableGrass();
+
+            WalkToTheGoal(walkToGoal);
         }
 
-        if (targetGrass != null)
+        if(!reachedGoal)
         {
-            MoveTowardsGrass();
+            if (!isTargeted)
+            {
+                FindNearestAvailableGrass();
+            }
+
+            if (targetGrass != null)
+            {
+                MoveTowardsGrass();
+            }
+            else if (IsPlayerInRange(playerFollowDistance))
+            {
+                FollowPlayer();
+            }
+
+            triggerWalkToGoal(alignToGoal);
         }
     }
 
@@ -55,8 +87,43 @@ public class SheepController : MonoBehaviour
         transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 
+    void FollowPlayer()
+    {
+        Vector2 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector2 direction = (playerPosition - (Vector2)transform.position).normalized;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    bool IsPlayerInRange(float range)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        return player != null && Vector2.Distance(transform.position, player.transform.position) <= range;
+    }
+
     public void SetIsTargeted(bool value)
     {
         isTargeted = value;
+
+        if (!isTargeted)
+        {
+            // Reset targetGrass if no longer targeting
+            targetGrass = null;
+        }
+    }
+
+    void triggerWalkToGoal(Transform target)
+    {
+        Vector3 direction = target.position - transform.position;
+        if (direction.magnitude < 2f)
+        {
+            reachedGoal = true; 
+        }
+    }
+
+    void WalkToTheGoal(Transform target)
+    {
+        Vector3 direction = target.position - transform.position;
+        if (direction.magnitude > 0.1f)
+            transform.Translate(moveSpeed * Time.deltaTime * direction.normalized);
     }
 }
