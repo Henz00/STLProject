7251f5bb3a11d8a9895422dd72525f7effe467b4
xkhassoukx <Oskha18@student.sdu.dk>
2023-11-17 08:59:57 +0100

    Grass random spawn

--- Assets/Scripts/GrassController.cs
+++ Assets/Scripts/GrassController.cs
@@ -7,9 +7,18 @@ public class GrassController : MonoBehaviour
     public float grassSpawnDelay = 2f;
     public bool isEnabled = true;
 
+    // Adjust these values to set the range for grass spawn
+    public float minX = -10f;
+    public float maxX = 6f;
+    public float minY = -2f;
+    public float maxY = 2f;
+
+    private Vector2 initialPosition;
+
     void Start()
     {
         gameObject.SetActive(isEnabled);
+        initialPosition = transform.position; // Save the initial position
         Invoke("GrassSpawnTime", grassSpawnDelay);
     }
 
@@ -25,6 +34,15 @@ public class GrassController : MonoBehaviour
 
     private void GrassSpawnTime()
     {
+        // Randomly set the offset within the specified range
+        float offsetX = Random.Range(minX, maxX);
+        float offsetY = Random.Range(minY, maxY);
+
+        // Calculate the new position based on the initial position
+        Vector2 newPosition = new Vector2(initialPosition.x + offsetX, initialPosition.y + offsetY);
+
+        transform.position = newPosition;
+
         isEnabled = !isEnabled;
         gameObject.SetActive(isEnabled);
     }
